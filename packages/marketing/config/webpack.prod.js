f70a5b35034e80b4af5d7d71ac084187647f2941
Rizaldi <rizaldi@stockbit.com>
2022-06-01 10:28:19 +0700

    ci: fix build webpack marketing

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicpath: '/marketing/latest/'
+    publicPath: '/marketing/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
