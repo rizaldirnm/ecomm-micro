ff87a1def7a213cbc1f2a05559fa8eeee9c795d1
Rizaldi <rizaldi@stockbit.com>
2022-06-01 10:25:31 +0700

    ci: marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,6 +7,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicpath: '/marketing/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
