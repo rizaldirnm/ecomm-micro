44af508a8e93b00f85e05a7982e7ca75e3e4582d
Rizaldi <rizaldi@stockbit.com>
2022-06-01 11:49:14 +0700

    feat: history isolation marketing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
 // @ Mount function to start up the app
-const mount = (element, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (element, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
 
   if (onNavigate) {
     // listen changes of history on marketing app
@@ -31,7 +31,9 @@ if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot, {});
+    mount(devRoot, {
+      defaultHistory: createBrowserHistory(),
+    });
   }
 }
 
